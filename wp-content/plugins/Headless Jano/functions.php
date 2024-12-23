325fbd4c4690ffd1a629e116fb46d2ca642cf8f6
SuperJan93 <janvanerkel92@gmail.com>
2024-12-23 11:24:39 +0100

    plugin

--- wp-content/plugins/Headless Jano/functions.php
+++ wp-content/plugins/Headless Jano/functions.php
@@ -58,7 +58,7 @@ add_action('login_enqueue_scripts', function() {
       // Vervang het logo
       echo '<style>
         .login h1 a {
-            background-image: url("https://admin.janvanerkel.nl/Jano-be/wp-content/uploads/login-logo.svg");
+            background-image: url("https://admin.janvanerkel.nl/Jano-be/wp-content/uploads/login-logo.svg") !important;
             background-size: contain;
             width: 100%;
             height: 80px; /* Pas de hoogte aan naar wens */
