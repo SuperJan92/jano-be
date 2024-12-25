66ef716a913aec1422a5b9820ffb6adcc1944494
SuperJan93 <janvanerkel92@gmail.com>
2024-12-25 20:06:38 +0100

    dotenv

--- wp-content/plugins/Headless Jano/functions.php
+++ wp-content/plugins/Headless Jano/functions.php
@@ -15,34 +15,6 @@ $dotenv->load();
 
 $api_key = $_ENV['MY_API_KEY'] ?? null;
 
-add_action('rest_api_init', function() {
-      // Voeg een filter toe om de authenticatie te controleren voor REST API-aanroepen
-      add_filter('rest_authentication_errors', function($result) {
-            // Controleer of het verzoek een REST API-aanroep is door te kijken naar de URI
-            if (strpos($_SERVER['REQUEST_URI'], '/wp-json/') === false) {
-                  return $result; // Geen validatie voor andere verzoeken zoals admin
-            }
-
-            // Haal de API-sleutel uit de HTTP headers
-            $api_key = isset($_SERVER['HTTP_X_API_KEY']) ? $_SERVER['HTTP_X_API_KEY'] : null;
-
-            // Als er geen API-sleutel is of deze onjuist is, geef een foutmelding terug
-            if (!$api_key || $api_key !== $_ENV['MY_API_KEY']) {
-                  return new WP_Error('rest_forbidden', 'Forbidden', array('status' => 403));
-            }
-
-            return $result; // Geen fout, doorgaan naar de REST API
-      });
-});
-
-// Zorg ervoor dat de admin-verzoeken geen interferentie ervaren van de API-sleutelvalidatie
-add_action('init', function() {
-      // Controleer of we in de admin omgeving zijn, zo ja, sla de validatie over
-      if (is_admin()) {
-            return;
-      }
-});
-
 // Redirect alle verzoeken naar de admin pagina, behalve de login
 add_action('template_redirect', function() {
       if (!is_admin() && !is_login_page()) {
