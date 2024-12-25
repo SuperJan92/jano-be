d04368bd65f347faf66e0c70ff3b8966355693f7
SuperJan93 <janvanerkel92@gmail.com>
2024-12-25 18:08:14 +0100

    dotenv

--- wp-content/plugins/Headless Jano/functions.php
+++ wp-content/plugins/Headless Jano/functions.php
@@ -24,7 +24,7 @@ if (file_exists(ABSPATH . '.env')) {
 }
 
 // Laad het .env bestand vanuit de root van de WordPress installatie
-$dotenv = Dotenv::createImmutable(ABSPATH . 'admin');
+$dotenv = Dotenv::createImmutable(ABSPATH . 'admin'); // Zorg ervoor dat je enkel 1 keer 'admin' gebruikt
 $dotenv->load();
 
 // Log de API-sleutel naar de debug.log
