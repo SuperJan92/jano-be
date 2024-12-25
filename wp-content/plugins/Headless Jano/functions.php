c50dcf3524bddb9ae1b343539d7654ad940b3197
SuperJan93 <janvanerkel92@gmail.com>
2024-12-25 19:57:11 +0100

    dotenv

--- wp-content/plugins/Headless Jano/functions.php
+++ wp-content/plugins/Headless Jano/functions.php
@@ -15,7 +15,24 @@ $dotenv->load();
 
 $api_key = $_ENV['MY_API_KEY'] ?? null;
 
+add_action('rest_api_init', function() {
+      add_filter('rest_authentication_errors', function($result) {
+            // Controleer of de aanvraag naar de REST API gaat
+            if (strpos($_SERVER['REQUEST_URI'], '/wp-json/') === false) {
+                  return $result; // Geen validatie voor de backend of andere verzoeken
+            }
 
+            // Haal de API-sleutel uit de HTTP headers
+            $api_key = isset($_SERVER['HTTP_X_API_KEY']) ? $_SERVER['HTTP_X_API_KEY'] : null;
+
+            // Als er geen API-sleutel is of deze onjuist is, geef een foutmelding terug
+            if (!$api_key || $api_key !== $_ENV['MY_API_KEY']) {
+                  return new WP_Error('rest_forbidden', 'Forbidden', array('status' => 403));
+            }
+
+            return $result; // Geen fout, door naar de REST API
+      });
+});
 
 // Redirect alle verzoeken naar de admin pagina, behalve de login
 add_action('template_redirect', function() {
