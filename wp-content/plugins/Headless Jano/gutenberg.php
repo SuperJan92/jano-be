25957cfc21154fe32f335d921b32a0bcff9f689f
SuperJan93 <janvanerkel92@gmail.com>
2024-12-26 11:20:42 +0100

    Blocks

--- wp-content/plugins/Headless Jano/gutenberg.php
+++ wp-content/plugins/Headless Jano/gutenberg.php
@@ -1,22 +1,26 @@
 <?php
 // Dit is een debugregel
-echo 'TABS';
+echo 'TAaaaaaaaBS';
 
 // Alleen je eigen blokken toestaan in de editor
 function disable_all_gutenberg_blocks_except_own( $allowed_blocks ) {
       // Voeg je eigen blokken toe aan de lijst van toegestane blokken
-      // Vervang 'my-plugin/hero' door de naam van je eigen blok
       return array(
-            'my-plugin/hero', // Voeg hier je eigen blokken toe
+            'blocks/hero', // Voeg hier je eigen blokken toe
       );
 }
 
+add_filter( 'allowed_block_types_all', 'disable_all_gutenberg_blocks_except_own' );
+
 // Verwijder de tabjes (Blocks, Patterns, Media) uit de Gutenberg zijbalk
 function remove_gutenberg_sidebar_tabs($editor_settings) {
-      // Schakel de tabjes uit
-      $editor_settings['disableTabs'] = ['blocks', 'patterns', 'media'];
-
+      // Verberg de 'Blocks', 'Patterns' en 'Media' tabs in de zijbalk
+      if ( isset( $editor_settings['sidebar'] ) ) {
+            $editor_settings['sidebar']['blocks'] = false;  // Verberg de Blocks tab
+            $editor_settings['sidebar']['patterns'] = false; // Verberg de Patterns tab
+            $editor_settings['sidebar']['media'] = false; // Verberg de Media tab
+      }
       return $editor_settings;
 }
 
-add_filter('block_editor_settings_all', 'remove_gutenberg_sidebar_tabs');
+add_filter( 'block_editor_settings_all', 'remove_gutenberg_sidebar_tabs' );
