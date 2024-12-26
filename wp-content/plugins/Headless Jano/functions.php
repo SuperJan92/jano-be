230a2bbe0039a78f257e8fc3ad15c48f87012356
SuperJan93 <janvanerkel92@gmail.com>
2024-12-26 11:06:51 +0100

    Blocks

--- wp-content/plugins/Headless Jano/functions.php
+++ wp-content/plugins/Headless Jano/functions.php
@@ -284,7 +284,5 @@ add_action('admin_head', function () {
       <?php
 });
 
-echo 'HAAAAAAALLOOOO';
-
 // Voeg het bestand hero.php toe als dat nog niet gebeurd is
 include_once( plugin_dir_path( __FILE__ ) . 'blocks/hero.php' );
