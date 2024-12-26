dfb611e84b4c75ddf67e655621d74fa8b723f86c
SuperJan93 <janvanerkel92@gmail.com>
2024-12-26 12:51:42 +0100

    Blocks

--- wp-content/plugins/Headless Jano/gutenberg.php
+++ wp-content/plugins/Headless Jano/gutenberg.php
@@ -1,4 +1,16 @@
 <?php
+function echo_all_registered_blocks() {
+      // Haal alle geregistreerde blokken op
+      $registered_blocks = get_registered_block_types();
+
+      // Loop door de geregistreerde blokken en echo de naam
+      foreach ( $registered_blocks as $block ) {
+            echo '<p>' . esc_html( $block->name ) . '</p>';
+      }
+}
+
+// Voeg de functie toe aan een actie die wordt uitgevoerd in de editor, bijvoorbeeld 'admin_notices'
+add_action( 'admin_notices', 'echo_all_registered_blocks' );
 // Verwijder de submenu's (Blocks, Patterns, Media) uit de admin sidebar
 function remove_gutenberg_admin_menu_items() {
       remove_submenu_page( 'edit.php?post_type=wp_block', 'edit.php?post_type=wp_block' );
