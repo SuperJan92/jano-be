d4e155f48cd5e7b5e4474adcf487a91b0638bb95
SuperJan93 <janvanerkel92@gmail.com>
2024-12-26 11:28:04 +0100

    Blocks

--- wp-content/plugins/Headless Jano/Blocks/hero.php
+++ wp-content/plugins/Headless Jano/Blocks/hero.php
@@ -1,1 +1,17 @@
 <?php
+// Hero block registratie
+function register_hero_block() {
+      // Controleer of ACF aanwezig is
+      if( function_exists('acf_register_block') ) {
+            acf_register_block(array(
+                  'name'            => 'hero',
+                  'title'           => __('Hero Block'),
+                  'description'     => __('Een hero block met ACF-velden'),
+                  'render_template' => '', // Geen template renderen, wordt later ingevuld via ACF
+                  'category'        => 'common',
+                  'icon'            => 'alignfull',
+                  'keywords'        => array( 'hero', 'header' ),
+            ));
+      }
+}
+add_action('acf/init', 'register_hero_block');
