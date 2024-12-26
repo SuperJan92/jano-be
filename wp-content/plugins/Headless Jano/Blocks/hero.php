935b2d56c0d6da4c568e4a362824adbbe3d4fd1b
SuperJan93 <janvanerkel92@gmail.com>
2024-12-26 10:48:39 +0100

    Hero block

--- wp-content/plugins/Headless Jano/Blocks/hero.php
+++ wp-content/plugins/Headless Jano/Blocks/hero.php
@@ -1,4 +1,15 @@
 <?php
+add_filter('allowed_block_types_all', function($allowed_blocks, $block_editor_context) {
+      // Controleer of we in de blokeditor zijn
+      if (!empty($block_editor_context->post)) {
+            return [
+                  'custom/hero', // Voeg hier je eigen blokken toe
+                  'custom/ander-block', // Voeg meer blokken toe als je wilt
+            ];
+      }
+
+      return $allowed_blocks;
+}, 10, 2);
 add_action('init', function() {
       register_block_type('custom/hero', array(
             'attributes' => array(
