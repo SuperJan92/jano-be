0aa4982d5c6edd9b499905cd8ce325eed27e07c5
SuperJan93 <janvanerkel92@gmail.com>
2024-12-26 12:03:48 +0100

    Blocks

--- wp-content/plugins/Headless Jano/gutenberg.php
+++ wp-content/plugins/Headless Jano/gutenberg.php
@@ -1,7 +1,7 @@
 <?php
 
 function disable_all_gutenberg_blocks_except_own( $allowed_blocks ) {
-      return array( 'Heroo' ); // Zorg ervoor dat je hier de juiste naam gebruikt
+      return array( 'heroo' ); // Zorg ervoor dat dit overeenkomt met de naam die je in acf_register_block gebruikt
 }
 add_filter( 'allowed_block_types_all', 'disable_all_gutenberg_blocks_except_own' );
 
