19b1c31814197f12e87ba313f80696417e40e9e2
SuperJan93 <janvanerkel92@gmail.com>
2024-12-26 12:41:31 +0100

    Blocks

--- wp-content/plugins/Headless Jano/gutenberg.php
+++ wp-content/plugins/Headless Jano/gutenberg.php
@@ -1,5 +1,18 @@
 <?php
+function wpdocs_allowed_block_types( $block_editor_context, $editor_context ) {
+      if ( ! empty( $editor_context->post ) ) {
+            return array(
+                  'core/paragraph',
+                  'core/heading',
+                  'core/list',
+                  'hero-block', // Voeg je eigen blok hier toe
+            );
+      }
 
+      return $block_editor_context;
+}
+
+add_filter( 'allowed_block_types_all', 'wpdocs_allowed_block_types', 10, 2 );
 
 // Verwijder de submenu's (Blocks, Patterns, Media) uit de admin sidebar
 function remove_gutenberg_admin_menu_items() {
