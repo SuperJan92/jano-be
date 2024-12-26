03785497c58407f5b618121d9e1847a4fde3d8e5
SuperJan93 <janvanerkel92@gmail.com>
2024-12-26 11:21:47 +0100

    Blocks

--- wp-content/plugins/Headless Jano/gutenberg.php
+++ wp-content/plugins/Headless Jano/gutenberg.php
@@ -1,12 +1,12 @@
 <?php
 // Dit is een debugregel
-echo 'TAaaaaaaaBS';
+echo 'TABS';
 
 // Alleen je eigen blokken toestaan in de editor
 function disable_all_gutenberg_blocks_except_own( $allowed_blocks ) {
       // Voeg je eigen blokken toe aan de lijst van toegestane blokken
       return array(
-            'blocks/hero', // Voeg hier je eigen blokken toe
+            'my-plugin/hero', // Voeg hier je eigen blokken toe
       );
 }
 
@@ -24,3 +24,13 @@ function remove_gutenberg_sidebar_tabs($editor_settings) {
 }
 
 add_filter( 'block_editor_settings_all', 'remove_gutenberg_sidebar_tabs' );
+
+// Verwijder de submenu's (Blocks, Patterns, Media) uit de admin sidebar
+function remove_gutenberg_admin_menu_items() {
+      // Verwijder de submenu items voor Gutenberg
+      remove_submenu_page( 'edit.php?post_type=wp_block', 'edit.php?post_type=wp_block' ); // Blokken
+      remove_submenu_page( 'edit.php?post_type=wp_pattern', 'edit.php?post_type=wp_pattern' ); // Patronen
+      remove_submenu_page( 'upload.php', 'upload.php' ); // Media
+}
+
+add_action( 'admin_menu', 'remove_gutenberg_admin_menu_items', 999 );
