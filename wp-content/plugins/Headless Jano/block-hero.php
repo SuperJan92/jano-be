dbc53e50a45e303a7a8cad52dba737b1056b1d96
SuperJan93 <janvanerkel92@gmail.com>
2024-12-26 12:50:35 +0100

    Blocks

--- wp-content/plugins/Headless Jano/block-hero.php
+++ wp-content/plugins/Headless Jano/block-hero.php
@@ -1,14 +1,11 @@
 <?php
 function register_hero_block() {
       if ( function_exists( 'acf_register_block' ) ) {
-            // Echo de padlocatie om te zien of het blok correct is ingesteld
-            echo '<p>Het pad voor de hero.php is: ' . plugin_dir_path( __FILE__ ) . 'hero.php</p>';
-
             acf_register_block(array(
-                  'name'            => 'heroblock',
+                  'name'            => 'heroblock', // Hier gebruik je een specifieke naam die niet conflicteert
                   'title'           => __('Hero Block'),
                   'description'     => __('Een hero block met ACF-velden'),
-                  'render_template' => plugin_dir_path( __FILE__ ) . 'hero.php',
+                  'render_template' => 'hero.php', // Zorg ervoor dat dit pad klopt
                   'category'        => 'common',
                   'icon'            => 'alignfull',
                   'keywords'        => array( 'hero', 'header' ),
