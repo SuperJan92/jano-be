4efa75d017b1fdf53aa5ee19289367889e3ca491
SuperJan93 <janvanerkel92@gmail.com>
2024-12-26 11:16:59 +0100

    Blocks

--- wp-content/plugins/Headless Jano/gutenberg.php
+++ wp-content/plugins/Headless Jano/gutenberg.php
@@ -12,3 +12,13 @@ function disable_all_gutenberg_blocks_except_own( $allowed_blocks ) {
 }
 
 add_filter( 'allowed_block_types_all', 'disable_all_gutenberg_blocks_except_own', 10, 1 );
+
+// Verwijder Blokken, Patronen en Media uit het WordPress menu
+function remove_gutenberg_menu_items() {
+      // Verwijder de 'Blokken' en andere menu-items
+      remove_menu_page( 'edit.php?post_type=wp_block' ); // Blokken
+      remove_menu_page( 'edit.php?post_type=wp_pattern' ); // Patronen
+      remove_menu_page( 'upload.php' ); // Media
+}
+
+add_action( 'admin_menu', 'remove_gutenberg_menu_items', 999 );
