5f14282f13720e11383cdd197fbe99f5620beb50
SuperJan93 <janvanerkel92@gmail.com>
2024-12-28 23:07:33 +0100

    About block

--- wp-content/plugins/Headless Jano/functions.php
+++ wp-content/plugins/Headless Jano/functions.php
@@ -285,4 +285,5 @@ add_action('admin_head', function () {
 });
 
 include_once(plugin_dir_path(__FILE__) . 'block-hero.php');
+include_once(plugin_dir_path(__FILE__) . 'block-about.php');
 include_once( plugin_dir_path( __FILE__ ) . 'gutenberg.php' );
